dbb314c79c0f575ab99e751cabb8dd56fcaf4369
THEBOSS0369 <anujkumsharma9876@gmail.com>
2024-08-08 14:26:15 +0530

    Fixed the code

--- app/page.js
+++ app/page.js
@@ -1,113 +1,158 @@
-import Image from "next/image";
+"use client"
 
+import React from "react";
+import { useState } from "react";
+import Clarifai from 'clarifai';
+import Head from "next/head";
+import { Button } from "@/components/ui/button"
+import { Input } from "@/components/ui/input"
+import { Image } from "@nextui-org/image";
+import '../app/global.css'
+
+
+//Calling the Api
+const app = new Clarifai.App({
+  apiKey: '0dea3b4cb82249d4852c997fc5cac6c2',
+});
+
+
+//Main Function
 export default function Home() {
+  //Declaring 
+  const defaultImageUrl = "https://nextui-docs-v2.vercel.app/images/hero-card-complete.jpeg";
+  const [imageUrl, setImageUrl] = useState(defaultImageUrl);
+  const [imageDetails, setImageDetails] = useState(null);
+  const [description, setDescription] = useState('');
+  const [error, setError] = useState('');
+
+
+  //function when press the anlayze button
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    setImageDetails(null);
+    setDescription('');
+    setError('');
+
+    try {
+      const response = await app.models.predict(Clarifai.GENERAL_MODEL, imageUrl);
+      const concepts = response.outputs[0].data.concepts;
+      setImageDetails(concepts);
+      setDescription(generateDescription(concepts));
+    }
+    catch (err) {
+      setError('Failed to analyze image. Please Try Again or contact the owner')
+    }
+  };
+
+  // description for different type of images generated
+  const generateDescription = (concepts) => {
+    const conceptNames = concepts.map((concept) => concept.name.toLowerCase());
+    let description = 'This Image contains ';
+
+    const peoples = ['man', 'woman', 'boy', 'girl'];
+    const animals = ['dog', 'cat', 'bird', 'horse', 'cow', 'sheep'];
+    const fruits = ['apple', 'banana', 'mango', 'orange', 'grape', 'strawberry'];
+    const vehicles = ['car', 'bike', 'bicycle', 'motorcycle', 'truck'];
+
+    const person = conceptNames.find((name) => peoples.includes(name));
+    const animal = conceptNames.find((name) => animals.includes(name));
+    const fruit = conceptNames.find((name) => fruits.includes(name));
+    const vehicle = conceptNames.find((name) => vehicles.includes(name));
+
+    if (person) {
+      description += person;
+      if (conceptNames.includes(smile)) {
+        description += ' smiling';
+      }
+      description += `a person, specifically a ${person}.`;
+    } else if (animal) {
+      description += `an animal, specifically a ${animal}.`;
+    } else if (fruit) {
+      description += `a fruit, specifically a ${fruit}.`;
+    } else if (vehicle) {
+      description += `a vehilce, specifically a ${vehicle}.`;
+    } else {
+      description += 'the following items: ' + conceptNames.join(', ') + '.';
+    }
+
+    return description.charAt(0).toUpperCase() + description.slice(1);
+  };
+
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">app/page.js</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
+    <div className="bg-neutral-950">
+      <div className="relative h-20 bg-black">
+        <div className="absolute bottom-0 left-0 right-0 h-[1px] bg-gradient-to-r from-transparent via-gray-500 to-transparent"></div>
+      </div>
+      <div className="mt-20 ">
+        <h1 className="text-6xl text-white font-bold text-center p-2 tracking-tight">Snap & Know :</h1>
+        <h1 className="text-6xl text-white font-bold text-center p-3 tracking-tight"><span className="gradient-text"> Your Image Insights</span> Hub.</h1>
+        <div className="flex text-white items-center justify-center">
+          <p
+            className="
+                        p-10
+                        text-center
+                        text-lg
+                    "
           >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+            Upload your image's link and instantly discover what's inside,
+            <br />
+            from<span className="font-bold gradient-text-green"> vibrant green veggies to Beautiful Mountains.</span>
+            <br />
+            Your photo, our analysis!
+          </p>
         </div>
       </div>
 
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-full sm:before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full sm:after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
+      <div className="flex items-center h-64 mx-auto w-full justify-center">
+        <div className="flex w-full max-w-sm mt-[-170px] items-center space-x-2">
+          <form onSubmit={handleSubmit}>
+            <Input className="ml-10 w-80 bg-black text-white "
+              type="text"
+              value={imageUrl}
+              onChange={(e) => setImageUrl(e.target.value)}
+              placeholder="Link of the Image"
+            />
+            <br></br>
+            <Button
+              className="ml-40 bg-white  text-black hover:bg-gray-300"
+              type="submit"
+            >
+              Analyze
+            </Button>
+          </form>
+        </div>
+      </div>
+      <div className="flex items-center p-8 justify-center mt-[-120px] min-h-screen bg-neutral-900">
+        <div className=" bg-neutral-800 max-w-xl  justify-center p-6 rounded-lg ">
+          <h1 className="text-white text-2xl font-bold">Details</h1>
+          <br />
+          <div className="max-w-lg rounded bg-neutral-700 overflow-hidden shadow-lg">
+            {imageUrl && (
+              <img className="w-full" src={imageUrl} alt="Sunset in the mountains" />
+            )}
+            <div className="px-6 py-4">
+              <div className="font-bold text-white text-xl mb-2">Image Description</div>
+              {description && (
+                <p className="text-white text-base">
+                  {description}
+                </p>
+              )}
+            </div>
+            {imageDetails && (
+              <div>
+                <div className="px-6 pt-4 pb-2">
+                  {imageDetails.map((detail) => (
+                    <span key={detail.id} className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">
+                      {`${detail.name} (${(detail.value * 100).toFixed(2)}%)`}
+                    </span>
+                  ))}
+                </div>
+              </div>
+            )}
+          </div>
+        </div>
       </div>
 
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800 hover:dark:bg-opacity-30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore starter templates for Next.js.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50 text-balance`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
+    </div>
   );
 }
